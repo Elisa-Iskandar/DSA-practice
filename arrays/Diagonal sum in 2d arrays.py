f4194d0b84d63a4b47bd7bafef0e552aaef3097d
Elisa <58130797+Elisa-Iskandar@users.noreply.github.com>
2023-10-17 20:34:04 +0800

    Update Diagonal sum in 2d arrays.py

--- arrays/Diagonal sum in 2d arrays.py
+++ arrays/Diagonal sum in 2d arrays.py
@@ -1,10 +1,8 @@
 #Given 2D list calculate the sum of diagonal elements.
 def diagonal_sum(matrix):
     sum = 0
-    for rows in range(len(matrix)):
-        for columns in range(len(matrix[0])):
-            if rows == columns:
-                sum += matrix[rows][columns]
+    for i in range(len(matrix)):
+        sum += matrix[i][i]
     return sum
 myList2D= [[1,2,3],[4,5,6],[7,8,9]] 
 print(diagonal_sum(myList2D))
