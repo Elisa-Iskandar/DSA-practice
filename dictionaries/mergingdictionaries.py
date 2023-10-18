507a6ef5d0b5057a38d88e9070610ae8b21356f0
Elisa <58130797+Elisa-Iskandar@users.noreply.github.com>
2023-10-18 17:59:02 +0800

    Update mergingdictionaries.py

--- dictionaries/mergingdictionaries.py
+++ dictionaries/mergingdictionaries.py
@@ -4,11 +4,8 @@ dict2 = {'b': 3, 'c': 4, 'd': 5}
 
 def merge_dicts(dict1, dict2):
     new_dict = dict1.copy()
-    for keys in dict2:
-        if keys in new_dict:
-            new_dict[keys] += dict2[keys]
-        else:
-            new_dict[keys] = dict2[keys]
+    for keys, value in dict2.items():
+        new_dict[keys] = new_dict.get(keys, 0) + value
     return new_dict
         
 print(merge_dicts(dict1, dict2))
